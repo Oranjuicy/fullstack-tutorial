6b6776911b8e72f72a7eb3f1941707e149daa132
Retief Ryke <retief.ryke@gmail.com>
2023-08-18 13:45:26 +0200

    List todos and through frontend querying

---
+++ src/Components/Todo.tsx
@@ -0,0 +1,27 @@
+import type { Todo } from "~/types";
+
+type TodoProps = {
+    todo: Todo
+}
+
+export default function Todo({ todo }: TodoProps) {
+    const {id, text, done} = todo
+
+    return (
+        <>
+        <div className="flex gap-2 items-center justify-between">
+            <div className="flex gap-2 items-center">
+                <input className="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:" 
+                type="checkbox" name="done" id="done" checked={done} />
+                <label htmlFor="done" className={'cursor-pointer'}>
+                    {text}
+                </label>
+            </div>
+            <button className="text-white bg-blue-700 hover:bg-blue-800 rounded focus:ring-4 focus:outline-no">
+                Delete</button>
+        </div>
+
+           
+        </>
+    )
+};
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,12 @@
+import type { inferRouterOutputs } from "@trpc/server";
 import { z } from "zod";
+import type { AppRouter } from "./server/api/root";
+import { Router } from "next/router";
+
+type RouterOuputs = inferRouterOutputs<AppRouter>;
+type allTodosOutput = RouterOuputs["todo"]["all"]
+
+export type Todo = allTodosOutput[number]
 
 export const todoInput  = z.string({
     required_error: "Add you todo item",
