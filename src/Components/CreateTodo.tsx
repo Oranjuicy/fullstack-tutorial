561b1cbaeb3f202073419160c5a51ccc83e0071c
Retief Ryke <retief.ryke@gmail.com>
2023-08-22 14:58:25 +0200

    Add todo toggle and delete functionality and update some style

--- src/Components/CreateTodo.tsx
+++ src/Components/CreateTodo.tsx
@@ -28,6 +28,8 @@ export default function CreateTodos(){
                 } 
 
                 mutate(newTodo)
+
+                setNewTodo("")
             }}
             className="flex gap-4">
             <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg"
--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,4 +1,5 @@
 import type { Todo } from "~/types";
+import { api } from "~/utils/api";
 
 
 type TodoProps = {
@@ -8,18 +9,38 @@ type TodoProps = {
 export default function Todo({ todo }: TodoProps) {
     const {id, text, done} = todo
 
+    const trpc = api.useContext()
+
+    const {mutate: doneMutation} = api.todo.toggle.useMutation({
+        onSettled: async () => {
+            await trpc.todo.all.invalidate()
+        }
+    })
+
+    const {mutate: deleteMutation} = api.todo.delete.useMutation({
+        onSettled: async () => {
+            await trpc.todo.all.invalidate()
+        }
+    })
+
 
     return (
         <>
         <div className="flex gap-2 items-center justify-between">
             <div className="flex gap-2 items-center">
-                <input className="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:" 
-                type="checkbox" name="done" id="done" checked={done} />
+                <input className="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50" 
+                type="checkbox" name="done" id="done" checked={done} 
+                onChange={(e) => {
+                    doneMutation({id, done: e.target.checked})
+                }}/>
                 <label htmlFor="done" className={'cursor-pointer'}>
                     {text}
                 </label>
             </div>
-            <button className="text-white bg-blue-700 p-1 hover:bg-blue-800 rounded focus:ring-4 focus:outline-no">
+            <button className="text-white bg-blue-700 p-1 hover:bg-blue-800 rounded focus:ring-4 focus:outline-no"
+             onClick={() => {
+                deleteMutation(id)
+            }}>
                 Delete</button>
         </div>
         </>
